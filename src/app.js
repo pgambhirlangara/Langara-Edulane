740c2e47282ac3be8edbf6cc3dd24f20dcb1fdc8
Hiroshi Isobe <isobe69b@gmail.com>
2021-10-19 17:01:46 -0700

    Installed Firebase. Configuration statement is at the top of app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+//---------- Firebase configuration starts from here
+import * as firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyAK10RzRApaDyJZfrs4NN-YSxaQvRZyOxg",
+  authDomain: "langara-edulane.firebaseapp.com",
+  projectId: "langara-edulane",
+  storageBucket: "langara-edulane.appspot.com",
+  messagingSenderId: "163537109728",
+  appId: "1:163537109728:web:76f7da334d591276b08685",
+  measurementId: "G-5125DLL6BZ"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+//---------- Firebase configuration ends here
+
 alert('Hello Langara Edulane');
