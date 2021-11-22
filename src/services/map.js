26dbe2594f8fefa8e85d2380f4b7cea8e2d3e8da
Hiroshi Isobe <isobe69b@gmail.com>
2021-11-22 10:31:09 -0800

    Implemented: the feature to get current position and set a marker

--- src/services/map.js
+++ src/services/map.js
@@ -1,8 +1,46 @@
 let map;
+let infoWindow;
+/* Set Initial position to the location of Langara college */
+let currentLatitude = 49.224909417743305;
+let currentLongitude = -123.10851741557572; 
+let mapArea = document.getElementById('map-area');
+let currentLatLang;
+const getCurrentPositionBtn = document.getElementById('getCurrentPosition')
+
+getCurrentPositionBtn.addEventListener('click', ()=> {
+  if ( navigator.geolocation ) {
+      	navigator.geolocation.getCurrentPosition( 
+         ( position ) => {  
+            currentLatitude = position.coords.latitude;
+            currentLongitude = position.coords.longitude;
+            initMap();
+            showCurrentPositionMarker();
+      	}, 
+		( error ) => {
+			if (	error.code == error.PERMISSION_DENIED ) {
+				window.alert("Geolocation Permission Denied");
+			}
+		 });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+})
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map-area"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  map = new google.maps.Map(mapArea, {
+    center: { lat: currentLatitude, lng: currentLongitude },
+    zoom: 19,
   });
+}
+
+const showCurrentPositionMarker = ()=> {
+  infoWindow = new google.maps.InfoWindow();
+   const position = {
+     lat: currentLatitude,
+     lng: currentLongitude,
+   };
+   infoWindow.setPosition(position);
+   infoWindow.setContent("You are here!");
+   infoWindow.open(map);
+   map.setCenter(position);
 }
