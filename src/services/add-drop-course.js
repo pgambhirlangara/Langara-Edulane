6961d2a012ada58bcc1ffffe32f0a28b7e2123d4
Prabhjyot <knowprabhjyot@gmail.com>
2021-11-24 14:13:33 -0800

    added relevant messages if course is not there

--- src/services/add-drop-course.js
+++ src/services/add-drop-course.js
@@ -13,29 +13,33 @@ const getCurrentCourseList = () => {
 }
 
 const generateAddDropCourseHTML = (data) => {
-   
-    data.forEach((val) => {
+    
+    if (data.length === 0) {
+        addCourseContainer.innerHTML = "You currently have no courses";
+    } else {
+        data.forEach((val) => {
 
-        val.doc.data().courses.forEach((courseVal) => {
-            let courseSubContainer = document.createElement('div');
+            val.doc.data().courses.forEach((courseVal) => {
+                let courseSubContainer = document.createElement('div');
+    
+                let courseLI = document.createElement('button');
+                const IdToDelete = val.doc.id;
+                let deleteButton = `<button class="accent-btn delete-btn" onclick="deleteCourseFromUser(${JSON.stringify(IdToDelete).split('"').join("&quot;")})"><i class="fas fa-trash"></i></button>`;
+    
+                courseLI.textContent = courseVal.name;
+                courseSubContainer.classList.add("add-course-sub-container");
+                courseLI.classList.add("accent-btn", "course-btn");
+                
+                courseSubContainer.appendChild(courseLI);
+                courseSubContainer.innerHTML += deleteButton;
+                addCourseContainer.appendChild(courseSubContainer);
+    
+            })
+    
+    
+        });
+    }
 
-            let courseLI = document.createElement('button');
-            const IdToDelete = val.doc.id;
-
-            let deleteButton = `<button class="accent-btn delete-btn" onclick="deleteCourseFromUser(${IdToDelete})"><i class="fas fa-trash"></i></button>`;
-
-            courseLI.textContent = courseVal.name;
-            courseSubContainer.classList.add("add-course-sub-container");
-            courseLI.classList.add("accent-btn", "course-btn");
-            
-            courseSubContainer.appendChild(courseLI);
-            courseSubContainer.innerHTML += deleteButton;
-            addCourseContainer.appendChild(courseSubContainer);
-
-        })
-
-
-    });
 }
 
 const deleteCourseFromUser = (id) => {
