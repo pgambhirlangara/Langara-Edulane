dc588d584285cf8ba71e1a3a818516cc43a661a5
anasnegur <91930145+anasnegur@users.noreply.github.com>
2021-11-23 18:30:55 -0800

    home screen - calendar part

--- src/services/calendar.js
+++ src/services/calendar.js
@@ -98,4 +98,17 @@ let params = {
     month: now.getMonth(),
     year: now.getFullYear()
 };
-document.getElementById('calendar').innerHTML = calendar(params);
+// document.getElementById('calendar').innerHTML = calendar(params);
+
+document.getElementById('calendar-add-btn').addEventListener('click', function (event) {
+
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('.click-me')) return;
+
+	// Don't follow the link
+	event.preventDefault();
+
+	// Log the clicked element in the console
+	console.log(event.target);
+
+}, false);
