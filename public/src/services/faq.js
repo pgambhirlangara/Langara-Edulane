9d2cce2ee20962e786a2893789889100d796e3b7
Hiroshi Isobe <isobe69b@gmail.com>
2021-11-27 11:59:54 -0800

    FAQ section styling sdjusted

--- public/src/services/faq.js
+++ public/src/services/faq.js
@@ -1,11 +1,10 @@
+const acc = document.getElementsByClassName("accordion");
+const FaqContents = document.getElementsByClassName("panel");
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
